acbf4a810a31774cd5ad8404aa668a2cc811bddf
Joshua Bowren <joshua.bowren@knights.ucf.edu>
2015-12-24 23:32:22 -0500

    Added new version of libraahn causing RAAHN to perform much better. Changed the configuration of XMapAutoencoder.xml to well with the libraahn changes.

--- raahnsimulation/Utils.cs
+++ raahnsimulation/Utils.cs
@@ -399,7 +399,7 @@ namespace RaahnSimulation
         public const string EXPERIMENT_FOLDER = "Data/Experiments/";
         public const string LOG_FOLDER = "Data/Logs/";
         public const string TIME_ELAPSED = "Total Time Elapsed: {0}s";
-        public const string VERSION_STRING = "Version 20151204";
+        public const string VERSION_STRING = "Version 20151224";
         //Log strings.
         public const string LOG_SCORE_FILE = "Scores.txt";
         public const string LOG_SCORE_FORMAT = "{0} ";
